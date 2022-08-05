06a587470adc93c4a82b58e3898c23a3ac9b14e0
williamcuesta <william09y@gmail.com>
2022-08-05 12:02:13 -0500

    change the time_zone for America/Bogota in settings>base file.

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,7 @@ env = environ.Env()
 DEBUG = env.bool('DJANGO_DEBUG', False)
 
 # Language and timezone
-TIME_ZONE = 'America/Mexico_City'
+TIME_ZONE = 'America/Bogota'
 LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 USE_I18N = True
@@ -134,7 +134,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.s
 # Admin
 ADMIN_URL = 'admin/'
 ADMINS = [
-    ("""Pablo Trinidad""", 'pablotrinidad@ciencias.unam.mx'),
+    ("""William Cuesta""", 'william09y@gmail.com'),
 ]
 MANAGERS = ADMINS
 
